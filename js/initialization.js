f8c0a24cb08c5bb01e9146002d8e305be27eb9b5
Johnrey Bacal <johnrey.bacal@yahoo.com>
2017-09-25 21:50:08 -0700

    improved asset attributes window

--- js/initialization.js
+++ js/initialization.js
@@ -20,39 +20,42 @@ function getAngle($this){
     return angle;
 }
 
+function displayAssetAttributes($this){
+	$('#selectedAsset').html($this.attr('id'));
+	// var $this = $(this);
+	var thisPos = $this.position();
+	var parPos = $this.parent().position();
+	var x = thisPos.left - parPos.left;
+	var y = thisPos.top - parPos.top;	
+	var angle = getAngle($this);
+	$('#pos').html("x: " + x + "<br />y: " + y);
+	$('#siz').html("w: " + $this.css('width') + "<br />h: " + $this.css('height'));
+	$('#ang').html(angle)	
+}
+
 function assetInteractability(id){
 	$('#' + id).resizable({
 		containment: "#workspace",		//para hanggang workspace lng ung laki
 		// animate: true, ghost: true,		    	
 		minHeight: 50, minWidth: 50,
 		resize: function(event, ui){
-			$('#siz').html("w: " + ui.size.width + "<br />h: " + ui.size.height);
+			displayAssetAttributes($('#' + id));
 		}
 		//handles: "n, e, s, w, nw, ne, sw, se"
 	}).draggable({
 		containment: "#workspace", 		//para di lumabas sa workspace
 		helper: "original", cursor: "move",
 		drag: function(){
-			var $this = $(this);
-			var thisPos = $this.position();
-			var parPos = $this.parent().position();
-			var x = thisPos.left - parPos.left;
-			var y = thisPos.top - parPos.top;
-			$('#pos').html("x: " + x + "<br />y: " + y);
+			displayAssetAttributes($('#' + id));
 		}
 	}).css("z-index", assets[currentPage].split('/').length - 1);	
+	$('.rotatable').rotatable({
+		rotate: function(){
+			displayAssetAttributes($('#' + id));
+		}
+	}).removeClass('rotatable');
 	$('#' + id).mousedown(function(){//gawing focusable lol kinuha ko lng ung id haha
-		$('#selectedAsset').html($(this).attr('id'));
-		var $this = $(this);
-		var thisPos = $this.position();
-		var parPos = $this.parent().position();
-		var x = thisPos.left - parPos.left;
-		var y = thisPos.top - parPos.top;
-		var $this = $(this);
-		var angle = getAngle($this);
-		$('#pos').html("x: " + x + "<br />y: " + y);
-		$('#siz').html("w: " + $this.css('width') + "<br />h: " + $this.css('height'));
-		$('#ang').html(angle)
+		displayAssetAttributes($(this));
 	});
 }
 
@@ -74,7 +77,6 @@ $(document).ready(function(){
 			$(ui.helper).removeClass("first");	//pra hindi mag-clone	
 			$(ui.helper).addClass("asset");
 			$(this).append($(ui.helper).clone().wrapInner('<div class = "rotatable rotate"></div>').attr('id', id));
-			$('.rotatable').rotatable().removeClass('rotatable');
 			assetInteractability(id);
     		$("#z-" + currentPage.toString()).prepend("<li id = \"" + id + "-z\">" + id + "</li>");
 		}
@@ -85,8 +87,7 @@ $(document).ready(function(){
 			var order = $(this).sortable("toArray");	//gawing array ung list na element
 			order.reverse();
 			for(var i = 0; i < order.length; i++){
-				$("#" + order[i].replace("-z", "")).css("z-index", i);	//ayusin ung z order
-				//alert(i + " = " + $("#" + i).css("z-index"));
+				$("#" + order[i].replace("-z", "")).css("z-index", i + 1);	//ayusin ung z order				
 			}
 		}
 	});
@@ -100,5 +101,12 @@ $(document).ready(function(){
 		$("#z-" + currentPage.toString()).show();
 		$("#currentPage").html($(this).attr('id'));
 	});
+
+
+	// $('div.asset').children('div.rotate').children('div.ui-rotatable-handle').mousedown(function(){
+	// 	var angle = getAngle($(this).parent().parent());
+	// 	$('#ang').html(angle);
+	// });	
+
 	/*End of initializations*/
 });
