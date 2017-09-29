0d3a094890b6856c4ca4ab95fc070018309db944
Angela <30515472+HimitsuShi@users.noreply.github.com>
2017-09-29 12:47:53 +0800

    Add files via upload

---
+++ application/views/includes/head.php
@@ -0,0 +1,21 @@
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="memstyle" href="../../cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+
+    <!-- fonts (???) -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- styles (!!!) -->
+    <link href="css/memstyle.css" rel="stylesheet">
