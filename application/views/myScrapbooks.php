199e09573379542d2a905401b8dff7b318e933c0
Nath Piquero <30515378+naytanyel@users.noreply.github.com>
2017-10-17 21:45:50 +0800

    Update myScrapbooks.php

--- application/views/myScrapbooks.php
+++ application/views/myScrapbooks.php
@@ -1,25 +1,58 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title><?php echo $title ?></title>
-</head>
-<body>
-	<p>Temporarily signed is as user 0001</p>
-	<form action = "<?php echo base_url('MemoRecap/editor/new'); ?>" method = "POST">
-		Name:<input type="text" name="name" required /><br />
-		Number of pages:<input type="number" name="pages" min = "3" max = "999" required /><br />
-		Size:<br />
-		<input type = "radio" name = "size" value = "512x768" required checked />512x768<br />
-		<input type = "radio" name = "size" value = "640x512" required />640x512<br />
-		<input type = "radio" name = "size" value = "512x512" required />512x512<br />
-		<input type = "radio" name = "size" value = "640x768" required />640x768<br />
-		<input type="submit" name="create" value = "Create" />
-	</form>
-	List of scrapbooks
-	<ol>
-		<?php
-			echo $list_of_scrapbooks;
-		?>
-	</ol>
-</body>
-</html>
+<div class="container">
+
+    <div class=" p-4 my-4 bg-faded">
+        <div class="row">
+            <div class="userstuff col-xs-12 col-sm-6 col-md-6">
+                <div class="well well-sm">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-4" style="padding:5%;">
+                            <img src = "<?php echo base_url('dp/'.$profile['dp']); ?>" alt="" class="img-rounded img-responsive" />
+                        </div>
+                        <div class="userstuff" style="margin:5% 0% 5% 25%;">
+                            <h4>User details</h4>							                       
+                            <p><?php echo $profile['name']; ?></p>
+                            <p><?php echo $profile['username']; ?></p>
+                            <div >
+                                
+                                    <a  href="<?php echo base_url('Account'); ?>" class="btn btn-default btn-lg" role="button" style="background-color:#e4b4b4; color:#4d2600;">Account Options</a>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container bg p-4 my-4" >
+
+    	<h2 class="landi hedest text-center ">List of Scrapbooks</h2><br />
+    	
+    		<div class=" wrapper" style ="width: 80%; margin: auto;">
+                <div class="row">   
+                  <div class="col-md-12">   
+                    <div class="table-responsive">
+                      <table class="table table-hover">
+                        <thead><tr><th>Name</th><th></th><th></th><th></th></tr></thead>
+                      <tbody>
+                      <?php foreach($list_of_scrapbooks as $display): ?>
+                        <tr>
+                          <td><?php echo $display['name'] ?></td>
+                          <td><?php echo $display['edit'] ?></td>
+                          <td><?php echo $display['view'] ?></td>
+                          <td><?php echo $display['delete'] ?></td>
+                        </tr>
+                      <?php endforeach; ?>
+                      </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+            </div>
+    	
+    	<!-- </center> -->
+    	<!-- <a href="<?php echo base_url('Home'); ?>" class="">Back to Home.</a> -->
+
+    </div>
+</div>
