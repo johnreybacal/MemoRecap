a69f820c8f3d453eda333fae6c2ce17daccf509a
Nath Piquero <30515378+naytanyel@users.noreply.github.com>
2017-10-17 21:47:23 +0800

    Update nav.php

--- application/views/includes/nav.php
+++ application/views/includes/nav.php
@@ -2,64 +2,63 @@
 <link rel="stylesheet" href="<?php echo base_url('css/index.css'); ?>">
 <style type="text/css">
 	.navbar #nav > .active > a {
-    color: #f0664c!important;
+    color: brown!important;
 
     /*dapat nandito kasi internal > external css
      lahat kasi sa index.css naka !important*/
 }
 
 </style>
+
 <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-overlay">
-		<a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none hed " href="<?php echo base_url('Home'); ?>">MemoRecap</a>
+		<!-- <a style="color:pink;" class="navbar-brand" href="<?php echo base_url('Home'); ?>">MemoRecap</a> -->
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
-		<ul id = "nav" class="navbar-nav mr-auto">
+	    <ul  class="navbar-nav navbar-center">    
 	        <li class="nav-item ">            
-	            <img class="smol" src="<?php echo base_url('css/images/logo_sana.png'); ?>"  alt="" />
-	        </li>
-	        <li class="nav-item  <?php echo (strpos(uri_string(), 'Home')>-1)?'active':''; ?>">
-				<a class=" nav-link  " href="<?php echo base_url('Home'); ?>">
+	            <a style="color:pink;"  href="<?php echo base_url('Home'); ?>">
+	            	<img class="smol" src="<?php echo base_url('css/images/pageLoader.gif'); ?>"  alt="" />
+	        	</a>
+	        </li> 
+	    </ul>
+		<ul id = "nav" class="navbar-nav nav-fill w-50 mx-auto">
+	        <li class="nav-item <?php echo (strpos(uri_string(), 'Home')>-1)?'active':''; ?>">
+				<a style="color:pink;" class="hed nav-link text-uppercase text-expanded " href="<?php echo base_url('Home'); ?>">
 					Home
 					<span class="sr-only">(current)</span>
 				</a>
 	        </li>
-		
-		<!-- <li class="nav-item px-lg-4">
-			<a class="hed nav-link text-uppercase text-expanded landi2 " href="">Create</a>
-        </li> 
-        <li class="nav-item px-lg-4 <?php echo (strpos(uri_string(), 'Assets')>-1)?'active':''; ?>" style="margin-left:20px;">
-			<a style="color:#dddddd;" class="hed nav-link text-uppercase text-expanded " href="<?php echo base_url('Assets'); ?>">
-				Box of Assets
-			</a>
-        </li>-->
-	        <li class="nav-item  <?php echo (strpos(uri_string(), 'Scrapbooks')>-1)?'active':''; ?>" >
-				<a class=" nav-link " href="<?php echo base_url('Scrapbooks'); ?>">
+			
+	        <li class="nav-item <?php echo (strpos(uri_string(), 'Scrapbooks')>-1)?'active':''; ?>" >
+				<a style="color:pink;"  class="hed nav-link text-uppercase text-expanded " href="<?php echo base_url('Scrapbooks'); ?>">
 					Scrapbooks
 				</a>
 	        </li>
-		
+			
 	        <li class="nav-item <?php echo (strpos(uri_string(), 'About')>-1)?'active':''; ?>" >
-				<a class=" nav-link " href="<?php echo base_url('About'); ?>">About</a>
+				<a style="color:pink;" class="hed nav-link text-uppercase text-expanded " href="<?php echo base_url('About'); ?>">About</a>
 	        </li>
-	    </ul>
-	        <form class="navbar-form navbar-left">
+   		</ul>
+    		 <form class="navbar-form navbar-left">
 			  <div class="input-group">
 			    <input type="text" class="form-control" placeholder="Search">
 			    <div class="input-group-btn">
 			      
-			       <button style="margin-left: 3px;" ty type="submit" class="btn btn-default">Submit</button>
+			       <button style="margin-left: 3px;" type="submit" class="btn btn-default">Submit</button>
 			      
 			    </div>
 			  </div>
 			</form>
-        <ul class="nav navbar-nav navbar-right">
+    	<ul class="nav navbar-nav nav-fill">
 			<li class="nav-item">
-				<a class="nav-link " href="<?php echo base_url('Login/')?>">Log In</a>
+				<a style="color:pink;" class="hed nav-link text-uppercase text-expanded " href="<?php echo base_url('Login/')?>">Log In</a>
 			</li>
+		<!-- </ul>
+		<ul class="nav navbar-nav  "> -->
 			<li class="nav-item">
-				<a class="nav-link " href="<?php echo base_url('Signup/')?>">Sign Up</a>
+				<a style="color:pink;" class="hed nav-link text-uppercase text-expanded " href="<?php echo base_url('Signup/')?>">Sign Up</a>
 			</li>
 		</ul>
 
@@ -80,11 +79,11 @@ sspan.onclick = function() {
 }
 </script>
 
- <script>
+<script>
      $(function() {
         $('#nav li a').click(function() {
-           $('#nav li').removeClass();
-           $($(this).attr('href')).addClass('active');
+           
+           $('#nav li').attr('href').addClass('active');
         });
      });
   </script>
