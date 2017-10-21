433b6416aaa2cf2f5d333d47f8c675c02ed5f27d
LoreReyes <30516891+lorejane@users.noreply.github.com>
2017-10-21 02:55:45 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,76 @@
 function deleteAsset(){
-	alert(assets[currentPage]);
-	var selectedAsset = $('#selectedAsset').html();			
-	$('#' + selectedAsset).remove();	//delete asset
+	// alert(assets[currentPage]);
+	var selectedAsset = $('#selectedAsset').html();				
+	$('#' + selectedAsset).fadeOut(1000, function(){//delete asset
+		$('#' + selectedAsset).remove();				
+	});
 	$('#' + selectedAsset + "-z").remove();		//delete on z-order
 	assets[currentPage] = assets[currentPage].replace(selectedAsset + "/", "");		//delete asset data
-	alert(assets[currentPage]);
+	$('.attr').html('');
+	sortZ($('#z-' + currentPage).sortable("toArray").reverse());
+	// alert(assets[currentPage]);
+}
+
+function sortZ(order){
+	for(var i = 0; i < order.length; i++){
+		$("#" + order[i].replace("-z", "")).css("z-index", i + 1);	//ayusin ung z order				
+	}
+}
+
+function sortPages(){
+	var order = $('#pagination').sortable("toArray");
+	var tempAssets = assets;
+	var temp = "";			
+	//alert($(this).sortable("toArray") + "   " + assets);			
+	for(var i = 0; i < order.length; i++){//add temporary id
+		var li = $('#' + order[i]);						//pagination
+		var li2 = $('#p-' + order[i].substring(5));		//page
+		var li3 = $('#z-' + order[i].substring(5));		//z-order
+		li.attr('id', 'temp' + i);
+		li2.attr('id', 'temppage' + i);
+		li3.attr('id', 'tempz' + i);
+		li.html(i + 1);
+	}
+	for(var i = 0; i < order.length; i++){//fix id
+		$('#temp' + i).attr('id', 'page-' + i);
+		$('#temppage' + i).attr('id', 'p-' + i);
+		$('#tempz' + i).attr('id', 'z-' + i);
+	}
+	for(var i = 0; i < order.length; i++){//sort assets
+		for(var j = 0; j < order.length - i - 1; j++){
+			var wtf = Number(order[j].substring(5));					
+			temp = tempAssets[wtf];				
+			tempAssets[wtf] = tempAssets[j];
+			tempAssets[j] = temp;					
+		}
+	}
+	var getBack = currentPage;
+	for(var p = 0; p <= pageCount; p++){
+		if(currentPage != p){
+			$("#p-" + currentPage).hide();
+			$("#z-" + currentPage).hide();
+			$("#p-" + p).show();
+			$("#z-" + p).show();
+		}
+		currentPage = p;				
+	}		
+	$("#p-" + currentPage).hide();
+	$("#z-" + currentPage).hide();
+	currentPage = getBack;
+	$("#p-" + currentPage).show();
+	$("#z-" + currentPage).show();	
 }
 
 $(document).ready(function(){    
 
+	$('#tools').sortable({
+		revert: 'invalid', scroll: false
+	});
+
 	$("#addPage").click(function(){
 		$("#workspace").append("<div id = \"p-" + pageCount + "\" class = \"pages ui-droppable\"></div>");
-		$("#pagination").append("<li id = \"page-" + pageCount + "\" class = \"page-button ui-sortable-handle\">" + (pageCount + 1) + "</li>");
-		$("#z-order-container").append("<ol reversed id = \"z-" + pageCount + "\" class = \"z_order\"></ol>");
+		$("#pagination").append("<li style='display: inline;' id = \"page-" + pageCount + "\" class = \"page-button ui-sortable-handle\">" + (pageCount + 1) + "</li>");
+		$("#z-order-container").append("<ol reversed id = \"z-" + pageCount + "\" class = \"z-order\"></ol>");
 		$("#page-" + pageCount.toString()).click(function(){	//para sa mga generated buttons
 			var page = $(this).attr("id");				//kunin ung pinindot
 			$("#p-" + currentPage.toString()).hide();	//hide page and z-order of current page
@@ -20,35 +78,44 @@ $(document).ready(function(){
 			currentPage = page.substring(5);			
 			$("#p-" + currentPage.toString()).show();	//show selected page and z-order
 			$("#z-" + currentPage.toString()).show();
-			$("#currentPage").html($(this).attr('id'));
+			$("#currentPage").html(Number(currentPage) + 1);
+			$('.attr').html('');
+			sortPages();
 		});
+
 		$("#z-" + pageCount.toString()).sortable({			//para sa generated na z-order
 			update: function(){
-				var order = $(this).sortable("toArray");
-				order.reverse();
-				for(var i = 0; i < order.length; i++){
-					$("#" + order[i].replace("-z", "")).css("z-index", i);
-				}
+				sortZ($(this).sortable("toArray").reverse());
 			}
-		});		
+		});
+
 		$("#p-" + pageCount.toString()).droppable({
 			accept: ".first",						//para sa generated pages
 			drop: function(event, ui){
-				var id = assetID + '-' + $('#wtf').html();				
+				var id = assetID + '-' + $('#wtf').html();	
+				var pos = $('#workspace').position();					
 	    		assets[currentPage] += id + "/";
 	    		assetID++;
 				$(ui.helper).removeClass("first");
 				$(ui.helper).addClass("asset");
-				$(this).append($(ui.helper).clone().wrapInner('<div class = "rotatable rotate"></div>').attr('id', id));				
+				$(this).append($(ui.helper).clone().wrapInner('<div class = "rotatable rotate"></div>').attr('id', id));
+				$('#' + id).css({position:"absolute", left:ui.offset.left-pos.left, top:ui.offset.top-pos.top});
 				assetInteractability(id);
 	    		$("#z-" + currentPage.toString()).prepend("<li id = \"" + id + "-z\">" + id + "</li>");
+	    		$("#z-" + currentPage).children('#' + id + '-z').mousedown(function(){
+					displayAssetAttributes($('#' + id));
+				});
+				assets[currentPage] += id + '/';
 			}
 		});		
-		$("#p-" + currentPage.toString()).hide();
-		$("#z-" + currentPage.toString()).hide();
-		$("#p-" + pageCount.toString()).show();
-		$("#z-" + pageCount.toString()).show();
+		$("#p-" + currentPage).hide();
+		$("#z-" + currentPage).hide();
+		$("#p-" + pageCount).show();
+		$("#z-" + pageCount).show();
 		currentPage = pageCount;
+		$('#p-' + currentPage).css({'background': hexToRgb($('#bgc').val())});
+		$('#p-' + currentPage).attr('data-bg', 'rgb');
+		$('#currentPage').html(Number(currentPage + 1));
 		assets[pageCount] = "";
 		pageCount++;
 	});		
@@ -56,69 +123,54 @@ $(document).ready(function(){
 	$('#delete-asset').click(function(){deleteAsset();});
 	
 	$('#delete-page').click(function(){
-		var selectedPage = $('#currentPage').html();
-		alert(selectedPage);
-		$('#' + selectedPage).remove();	//delete pagination
-		$('#p-' + selectedPage.substring(5)).remove();		//delete page
-		$('#z-' + selectedPage.substring(5)).remove();		//delete z-order		
-		assets.splice(selectedPage.substring(5), 1)		//delete asset data
-		for(var i = selectedPage.substring(5); i < pageCount; i++){//fix pagination
-			$('#page-' + i).html(i);
-			$('#page-' + i).attr('id', 'tempPage');			
-			$('#tempPage').attr('id', 'page-' + (i - 1));			
-		}
-		pageCount--;
-	});
-
-	$('#pagination').sortable({
-		containment: "#pagination-container",
-		update: function(){
-			var order = $(this).sortable("toArray");
-			var tempAssets = assets;
-			var temp = "";			
-			//alert($(this).sortable("toArray") + "   " + assets);			
-			for(var i = 0; i < order.length; i++){//add temporary id
-				var li = $('#' + order[i]);						//pagination
-				var li2 = $('#p-' + order[i].substring(5));		//page
-				var li3 = $('#z-' + order[i].substring(5));		//z-order
-				li.attr('id', 'temp' + i);
-				li2.attr('id', 'temppage' + i);
-				li3.attr('id', 'tempz' + i);
-				li.html(i + 1);
+		if(pageCount > 1){			
+			$('#page-' + currentPage).remove();	//delete pagination
+			$('#p-' + currentPage).remove();		//delete page
+			$('#z-' + currentPage).remove();		//delete z-order		
+			assets.splice(currentPage, 1)		//delete asset data
+			for(var i = currentPage; i < pageCount; i++){//fix pagination
+				$('#page-' + i).html(i);
+				$('#page-' + i).attr('id', 'tempPage');			
+				$('#tempPage').attr('id', 'page-' + (i - 1));
+				$('#p-' + i).attr('id', 'tempPage');
+				$('#tempPage').attr('id', 'p-' + (i - 1));
+				$('#z-' + i).attr('id', 'tempPage');
+				$('#tempPage').attr('id', 'z-' + (i - 1));
 			}
-			for(var i = 0; i < order.length; i++){//fix id
-				$('#temp' + i).attr('id', 'page-' + i);
-				$('#temppage' + i).attr('id', 'p-' + i);
-				$('#tempz' + i).attr('id', 'z-' + i);
+			pageCount--;
+			if(currentPage == pageCount){
+				currentPage--;
 			}
-			for(var i = 0; i < order.length; i++){//sort assets
-				for(var j = 0; j < order.length - i - 1; j++){
-					var wtf = Number(order[j].substring(5));					
-					temp = tempAssets[wtf];				
-					tempAssets[wtf] = tempAssets[j];
-					tempAssets[j] = temp;					
-				}
-			}
-			var getBack = currentPage;
-			for(var p = 0; p <= pageCount; p++){
-				if(currentPage != p){
-					$("#p-" + currentPage).hide();
-					$("#z-" + currentPage).hide();
-					$("#p-" + p).show();
-					$("#z-" + p).show();
-				}
-				currentPage = p;				
-			}		
-			$("#p-" + currentPage).hide();
-			$("#z-" + currentPage).hide();
-			currentPage = getBack;
-			$("#p-" + currentPage).show();
-			$("#z-" + currentPage).show();	
+			$('#p-' + currentPage).show();
+			$('#z-' + currentPage).show();
+			$("#currentPage").html(Number(currentPage) + 1);
+		}else{
+			alert("NANI???");
 		}
 	});
 
 	$('#changeBG').click(function(){		
-		$('#p-' + currentPage).css({'background':'rgb('+$('#R').val()+', '+$('#G').val()+', '+$('#B').val()+')'});
+		$('#p-' + currentPage).css({'background': hexToRgb($('#bgc').val())});
+		$('#p-' + currentPage).attr('data-bg', 'rgb');
+	});
+
+	$('.bgcolorbtn').click(function(){
+		var hex = $(this).data('color');
+		$('#p-' + currentPage).css({'background': hexToRgb(hex)});
+		$('#p-' + currentPage).attr('data-bg', 'rgb');
+	})
+
+	$('#bgc').change(function(){  
+	    $('#hexcolor').html($(this).val());
 	});
 
 });
+
+function hexToRgb(hex){
+    hex = hex.replace('#','');
+    r = parseInt(hex.substring(0,2), 16);
+    g = parseInt(hex.substring(2,4), 16);
+    b = parseInt(hex.substring(4,6), 16);
+    result = 'rgb('+r+','+g+','+b+')';    
+    return result;
+}						
