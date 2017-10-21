085d7c8a8b7d3d9777fdacbfcb3176f30fe727c8
Nath Piquero <30515378+naytanyel@users.noreply.github.com>
2017-10-22 03:16:38 +0800

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -47,75 +47,77 @@ body, html {
         background-attachment: scroll;
     }
 }
+  .pointer{
+    cursor: pointer;
+  }
+  .user{
+    color: #576b95;
+  }
+  .left{
+    float: left;
+  }
+  .right{
+    float: right;
+  }
+  .size{
+    font-size: 4%;
+  }
+  .stepup {
+    background: #73AD21;
+height: 5em;
+width: 12em;
+-moz-border-radius: 1em 4em 1em 4em;
+border-radius: 1em 4em 1em 4em;
+  }
+  .stepdown {
+    background: #73AD21;
+height: 65px;
+width:160px;
+-moz-border-radius: 25px 10px / 10px 25px;
+border-radius: 25px 10px / 10px 25px;
+}
 </style>
 <div class="bgimg-1 w3-display-container w3-opacity-min" >
   <div class="w3-display-middle" style="white-space:nowrap;">
-    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">Memo <span class="w3-hide-small">Recap</span> </span>
+    <!-- <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">Memo <span class="w3-hide-small">Recap</span> </span> -->
+    <!--style="background-color: rgba(236,161,166,0.5);"--> 
+    <div> 
+      <button id="myBtn" class="hed landi" style="padding:10px 55px;margin:0 0 0 43%;font-size:3em;border-radius:15px;background-color:#ffe6b3;">
+        Create
+      </button>
+    </div>
+      <h1 class="hedest landi">Create your own Scrapbooks in three easy steps</h1>
+      
+      <h4 class="stepup">sda</h4>
+      <h4 class="stepdown">da</h4>
+
+      <h4 class="stepup">da</h4>
+      <h4 class="stepdown">da</h4> 
+
+      <h4 class="stepup">da</h4>
+      <h4 class="stepdown">da</h4>
+    
   </div>
 </div>
 <div class="container" style="margin-top: 100px;">
-    <div class="row" >
-		<!--<div class="container bg-overlay col-md-4" style="margin:3% 0% 3% 5%; width:350px; height:350px;">
-         CAROUSEL 
-        
-		<div id="carouselExampleIndicators" class="carousel slide" style="padding:25px 10px 0px 10px;" data-ride="carousel">
-			<ol class="carousel-indicators">
-				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-			</ol>
-        <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active">
-				<img class="d-block img-fluid w-100" src = "<?php echo base_url('css/images/yato.gif'); ?>" alt="">
-				<div class="carousel-caption d-none d-md-block">
-					<h3 class="text-shadow">Keeping up</h3>
-					<p class="text-shadow">with every season.</p>
-				</div>
-            </div>
-            <div class="carousel-item">
-				<img class="d-block img-fluid w-100" src = "<?php echo base_url('css/images/yato3.gif'); ?>" alt="">
-				<div class="carousel-caption d-none d-md-block">
-					<h3 class="text-shadow">Have fun</h3>
-					<p class="text-shadow">customizing stuff.</p>
-				</div>
-            </div>
-            <div class="carousel-item">
-				<img class="d-block img-fluid w-100" src = "<?php echo base_url('css/images/yato4.gif'); ?>" alt="">
-				<div class="carousel-caption d-none d-md-block">
-					<h3 class="text-shadow">Digital</h3>
-					<p class="text-shadow">basic, chill, easy.</p>
-				</div>
-            </div>
-			</div>
-				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
-			</div>
-		</div>
-		-->
-		
+    <div class="row" >		
 		<div class="col-sm-6 col-md-6 container">
-			<!-- <img style="width:500px; height:150px; padding-left:25px;" src = "<?php echo base_url('css/images/TAYTEL.png'); ?>" alt=""/> -->
-			
-			
+		
 		<div class="tagline-lower text-expanded text-shadow " style="margin-left:25%;">
 			<p  style="font-size: 1em text-align:center;">&nbsp;Create. Share. Inspire.
 			<br />Unleash the Artist within.</p>
 		</div>	</div>
 		
 		
-	<div style="margin:1% 0% 1% 40%;"><button id="myBtn" class="hed landi"
-	style="font-size:3em;padding:10px 55px 10px 55px;
-	border-radius:15px;
-	background-color:#ffe6b3;
-	margin:auto;">
-	Create
-	</button></div>
+	<!-- <div style="margin:1% 0% 1% 40%;">
+    <button id="myBtn" class="hed landi"
+        	style="font-size:3em;padding:10px 55px 10px 55px;
+        	border-radius:15px;
+        	background-color:#ffe6b3;
+        	margin:auto;">
+	         Create
+	   </button>
+   </div> -->
 </div> 
 
 	
@@ -127,7 +129,7 @@ body, html {
     <div class="modal-header" style="background-color:#ffe6b3;">
       <span id = "xclose" class="close">&times;</span>
       <center>
-	      <h2 class="hed" style="color: #705239;">Enter scrapbook details</h2>
+	      <h2 class="hed" style="color: #705239;">Enter Scrapbook Details</h2>
 	  </center>
     </div>
     <div class="modal-body">
@@ -164,161 +166,54 @@ body, html {
   </div>
 </div>
 
-	  
-	<!-- BAAAADEEEEEEHHH  -->
-	<div class=" "> <!--style="background-color: rgba(236,161,166,0.5);"--> 
-	
-			<div class=" p-4 my-4"> <!-- style="background-color:#f4d7d7;" -->
-			<h1 class="hedest landi">Create your own Scrapbooks in three easy steps</h1>
-		
-			<div class=" p-4 my-4">
-				
-			<div class="row">
-						<div class="col-md-4 ">
-							<img class="card-img-top" style="height:100%;" src = "<?php echo base_url('css/images/step1.png'); ?>" alt=""/>
-						</div>
-						<div class="col-md-4 ">
-							<img class="card-img-top" style="height:100%;" src = "<?php echo base_url('css/images/step2.png'); ?>" alt="">			
-						</div>
-						<div class="col-md-4">
-							<img class="card-img-top" style="height:100%;" src = "<?php echo base_url('css/images/step3.png'); ?>" alt="">
-						</div>
-					</div>
-			</div>
-	</div>
-		<!-- 1st SECTION  -->
-		</div>
 
 <div class="bgimg-2 w3-display-container w3-opacity-min">
   <div class="w3-display-middle">
-    <span class="w3-xxlarge w3-text-white w3-wide">Scrapbooks</span>
+    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">Scrapbooks</span>
   </div>
 </div>
 
-<!-- <div id="section2" class="container-fluid">
-    <div class="row">
-      <div class="col-md-6" style="border: 1px solid blue;">
-        <h1>Recent</h1>
-        <div class="col-md-8">
-        	
-        <img class="roundrec" src = "<?php echo base_url('css/images/ep2.jpg'); ?>" alt="Sample image"/>
-        </div>
-        <div class="row">
-          <div class="col-md-2"></div>
-          <div class="col-md-6">
-            <a href="#">Username</a>
-          </div>
-          <div class="col-md-3" >
-            <p style="float: right">heart votes</p>
-          </div>
-        </div>
+  
+  <!-- might use w3-margin-bottom na class -->
+    <div class="w3-row-padding w3-center" style="margin: 5% 0 0 0;">
+      <div class="card-img-overlay">
+        <h2 class="card-title text-shadow text-white text-uppercase mb-0">Featured Works</h2>
+        <p class="parabooks text-shadow text-white">The works featured here have the most views/likes by fellow users of this website,
+      all of which shows aesthetics and life.
+      </p>
         
-        <div class="col-md-8 text-center" style="border: 1px solid yellow">
-          <h2>Scrap Title</h2>
-          <p>Scrapdescgfkbdbhgghbhgfugvdfgf
-          gkhdbdghjdghaidgvdifgdiavg
-          gdjlbdgfgbfdgfblgbgg</p>
-        </div>
-        <div>
-          <h2>Browse</h2>
-
-        </div>
       </div>
-     
-      <div class="col-md-6" style="border: 1px solid red;">
-        
-        <h1>Featured Work</h1>
-        
-        <img class="roundrec" src = "<?php echo base_url('css/images/ep1.jpg'); ?>" alt=""/>
-        <div class="row">
-          <div class="col-md-6">
-            <a href="#">Username</a>
-          </div>
-          <div class="col-md-3" >
-            <p style="float: right;">heart votes</p>
-          </div>
-        </div>
-        <div class="col-md-8 text-center" style="border: 1px solid yellow">
-          <h2>Scrap Title</h2>
-          <p>Scrapdesc</p>
-        </div>
-        <div>
-          <h2>Browse</h2>
-
-        </div>
-      </div>
-
-      
-    </div>
-  </div>
-   -->
-    <!-- might use w3-margin-bottom na class -->
-    <div class="w3-row-padding w3-center">
-      <h1>Recent</h1>
-      <div class="w3-col m4">
-        <img src = "<?php echo base_url('css/images/ep1.jpg'); ?>" style="width:80%" onclick="onClick(this)" class="pointer w3-round-xlarge" alt="The mist over the mountains">
+     <?php foreach($featured_works as $fw): ?>
+      <div class="w3-col m4 w3-border" style="background-color: #fefefe!important;">
+        <img src = "<?php echo $fw['first_page']; ?>" style="width:80%" onclick="onClick(this)" class="pointer w3-round-xlarge" alt="The mist over the mountains">
         <div class="w3-container">
-        <h4><b>Title</b></h4>
-        <h6>Owner</h6>
+        <h3 class="w3-margin-top"><b><?php echo $fw['name']; ?></b></h3>
+         <h1><a class="user left" href = "<?php echo base_url('Profile/'.$fw['username']); ?>"><?php echo $fw['username']; ?></a></h1>
           <div class="row">
-            <div class="w3-col m6" style="float: left;">          
-              <p>likes</p>
-            </div>
-            <div>
-              <p>views</p>
+            <div class="w3-col m12" style="float: left;">          
+              <h3 >
+                <span class="glyphicon glyphicon-heart-empty left" style="color: red;"></span>
+                <span class="w3-margin-left left" style="color: #111111;"><?php echo $fw['likes']; ?></span>
+                <span class="glyphicon glyphicon-eye-open right w3-margin-left" style="color: black;"></span>
+                <span class="right"><?php echo $fw['view_counter']; ?></span>&nbsp;</h3>
             </div>
           </div>
           <div>
-            <h5>Description</h5>
+            <h5><i><?php echo $fw['description']; ?></i></h5>
           </div>
         </div>
       </div>
+    <?php endforeach; ?>  
+<!-- end of 1st -->
 
-      <div class="w3-col m4">
-        <img src = "<?php echo base_url('css/images/p2.jpg'); ?>" style="width:80%" onclick="onClick(this)" class="pointer w3-round-xlarge" alt="Coffee beans">
-        <div class="w3-container">
-          <h4><b>Title</b></h4>
-          <p>Desc</p>
-        </div>
-      </div>
-
-      <div class="w3-col m4">
-        <img src = "<?php echo base_url('css/images/p3.jpg'); ?>" style="width:80%" onclick="onClick(this)" class="pointer w3-round-xlarge" alt="Bear closeup">
-        <div class="w3-container">
-          <h4><b>Title</b></h4>
-          <p>Desc</p>
-        </div>
-      </div>
-
-       <div class="w3-col m4">
-        <img src = "<?php echo base_url('css/images/ep1.jpg'); ?>" style="width:80%" onclick="onClick(this)" class="pointer w3-round-xlarge" alt="The mist over the mountains">
-        <div class="w3-container">
-          <h4><b>Title</b></h4>
-          <p>Desc</p>
-        </div>
-      </div>
-
-      <div class="w3-col m4">
-        <img src = "<?php echo base_url('css/images/p2.jpg'); ?>" style="width:80%" onclick="onClick(this)" class="pointer w3-round-xlarge" alt="Coffee beans">
-        <div class="w3-container">
-          <h4><b>Title</b></h4>
-          <p>Desc</p>
-        </div>
-      </div>
-
-      <div class="w3-col m4">
-        <img src = "<?php echo base_url('css/images/p3.jpg'); ?>" style="width:80%" onclick="onClick(this)" class="pointer w3-round-xlarge" alt="Bear closeup">
-        <div class="w3-container">
-          <h4><b>Title</b></h4>
-          <p>Desc</p>
-        </div>
-      </div>
    </div>
+
 
    <div class="bgimg-3 w3-display-container w3-opacity-min">
   <div class="w3-display-middle">
-    <span class="w3-xxlarge w3-text-white w3-wide">About Us</span>
+    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">About Us</span>
   </div>
+
 </div>
 
 
@@ -328,7 +223,7 @@ body, html {
 <!-- Modal for full size images on click-->
 <div id="modal01" class="w3-modal " onclick="this.style.display='none'">
   <span class="w3-button w3-large w3-black w3-display-topright" style="margin-top: 60px;" title="Close Modal Image"><i class="fa fa-remove"></i></span>
-  <div class="w3-modal-content w3-animate-opacity w3-display-right w3-transparent ">
+  <div class="w3-modal-content w3-animate-opacity w3-transparent ">
     <img id="img01" class="w3-round-xlarge w3-image" style="width: auto;">
     <p id="caption" class="w3-opacity w3-large"></p>
   </div>
@@ -336,16 +231,15 @@ body, html {
 
   <div style="position:relative;">
     <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
-      <p>Memorecap is blah blah blah blah.</p>
+      <p ><h1> MemoRecap</h1> <span style="font-size: 22px;">(Memory Recaptured) is a website where users can freely mess around with their photos and recreate something refreshing; where they can express their artistic side and bring art justice to the sceneries or persons they have captured with their camera lense; where they can easily revisit their memories in a unique way. Remember, Redesign, Recapture.</span></p>
     </div>
   </div>
-
-  <div class="bgimg-4 w3-display-container w3-opacity-min">
+<div class="bgimg-4 w3-display-container w3-opacity-min">
   <div class="w3-display-middle">
-    <span class="w3-xxlarge w3-text-white w3-wide">About Us</span>
+    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">Contact Us</span>
   </div>
 </div>
-
+  
 <script type="text/javascript">
 	function onClick(element) {
   document.getElementById("img01").src = element.src;
