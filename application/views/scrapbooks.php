56e7832664af4268e2e2c70a948e973719c4bef0
Nath Piquero <30515378+naytanyel@users.noreply.github.com>
2017-10-21 23:38:58 +0800

    Add files via upload

--- application/views/scrapbooks.php
+++ application/views/scrapbooks.php
@@ -1,69 +1,192 @@
-<img class="d-block img-fluid w-50 titel" src = "<?php echo base_url('css/images/TAYTEL.png'); ?>" alt="" >
-<div class="tagline-lower text-center text-expanded text-shadow ">
-	<p style="font-size: 25px">Create. Share. Inspire.
-	<br />Unleash the Artist within.</p>
-</div>
-	
-	
-	<!-- BAAAADEEEEEEHHH  -->
-	
-<div class="container bg p-4 my-4">
+<br/>
+<style type="text/css">
+	.pointer{
+		cursor: pointer;
+	}
+  .user{
+    color: #576b95;
+  }
+  .left{
+    float: left;
+  }
+  .right{
+    float: right;
+  }
+  .size{
+    font-size: 4%;
+  }
+  .bgimg-4, .bgimg-5, .bgimg-6 {
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
 
-  <div class=" p-4 my-4" style="margin:0px 85px 0px 85px; padding-top:25%;">
-  
-    <div class="card card-inverse">
-	<div class="pic row">
-					<div class="col-md-4 mb-4 mb-md-0">
-						<img class="card-img-top" src = "<?php echo base_url('css/images/ep1.jpg'); ?>" alt=""/>
-					</div>
-					<div class="col-md-4 mb-4 mb-md-0">
-						<img class="card-img-top" src = "<?php echo base_url('css/images/ep2.jpg'); ?>" alt="">			
-					</div>
-					<div class="col-md-4">
-						<img class="card-img-top" src = "<?php echo base_url('css/images/ep3.jpg'); ?>" alt="">
-					</div>
-				</div>
-      <div class="card-img-overlay bg-overlay">
-        <h2 class="card-title text-shadow text-white text-uppercase mb-0">Editors Picks</h2>
-        <p class="parabooks text-shadow text-white">The works featured here are chosen by the editors as those best shows
-			or endorses the beauty and functionalities of this website.
-			</p>
-		<a href="<?php echo base_url('Scrapbooks/Editors_Pick'); ?>" class="btn btn-secondary">View this gallery!</a>
+/* First image (Logo. Full height) */
+.bgimg-4 {
+    background-image: url('css/images/4.jpg');
+    min-height: 100%;
+}
+
+/* Second image (Portfolio) */
+.bgimg-5 {
+    background-image: url("css/images/5.jpg");
+    min-height: 400px;
+}
+
+/* Third image (Contact) */
+.bgimg-6 {
+    background-image: url("css/images/6.jpg");
+    min-height: 400px;
+}
+
+.w3-wide {letter-spacing: 10px;}
+.w3-hover-opacity {cursor: pointer;}
+
+/* Turn off parallax scrolling for tablets and phones */
+@media only screen and (max-device-width: 1024px) {
+    .bgimg-4, .bgimg-5, .bgimg-6 {
+        background-attachment: scroll;
+    }
+}
+</style>	
+<!-- start of editors -->
+	<div class="w3-row-padding w3-center" style="margin: 7% 0 0 0;">
+      <div class="card-img-overlay ">
+	        <h2 class=" text-shadow text-white text-uppercase ">Editors Picks</h2>
+	        <p class="parabooks text-shadow text-white">The works featured here are chosen by the editors as those best shows
+				or endorses the beauty and functionalities of this website.
+				</p>
+			
+		</div>
+      <?php foreach($featured_works as $ep): ?>
+      <div class="w3-col m4 w3-border" style="background-color: #fefefe!important;">
+        <img src = "<?php echo $ep['first_page']; ?>" style="width:80%" onclick="onClick(this)" class="pointer w3-round-xlarge" alt="The mist over the mountains">
+        <div class="w3-container w3-margin-top">
+        <h3><b><?php echo $ep['name']; ?></b></h3>
+         <h1><a class="user left" href = "<?php echo base_url('Profile/'.$ep['username']); ?>"><?php echo $ep['username']; ?></a></h1>
+          <div class="row">
+            <div class="w3-col m12" style="float: left;">          
+              <h3 >
+                <span class="glyphicon glyphicon-heart-empty left" style="color: red;"></span>
+                <span class="w3-margin-left left" style="color: #111111;"><?php echo $ep['likes']; ?></span>
+                <span class="glyphicon glyphicon-eye-open right w3-margin-left" style="color: black;"></span>
+                <span class="right"><?php echo $ep['view_counter']; ?></span>&nbsp;</h3>
+            </div>
+          </div>
+          <div>
+            <h5><i><?php echo $ep['description']; ?></i></h5>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    <?php endforeach; ?> 
+    <a  class="right w3-xlarge w3-black w3-hover-white w3-wide w3-animate-opacity" href="<?php echo base_url('Scrapbooks/Editors_Pick'); ?>">View this gallery!</a>
+<!-- end of 1st -->
+   </div> <!--close tag -->
 
-  <div class=" p-4 my-4" style="margin:0px 85px 0px 85px;">
-    <div class="card card-inverse">
-	<div class="pic row">
-					<div class="col-md-4 mb-3 mb-md-0"> <img class="card-img-top" src = "<?php echo base_url('css/images/FW1.jpg'); ?>" alt=""> </div>
-					<div class="col-md-4 mb-3 mb-md-0"> <img class="card-img-top" src = "<?php echo base_url('css/images/FW2.jpg'); ?>" alt=""> </div>
-					<div class="col-md-4 mb-3 mb-md-0"> <img class="card-img-top" src = "<?php echo base_url('css/images/FW3.jpg'); ?>" alt=""> </div>
-				</div>
-      <div class="card-img-overlay bg-overlay">
+
+<div class="bgimg-6 w3-display-container w3-opacity-min">
+  <div class="w3-display-middle">
+    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">Featured<span class="w3-hide-small">&nbsp;</span>Scrapbooks</span>
+    
+  </div>
+</div>
+   <!-- start of featured -->
+
+   <div class="w3-row-padding w3-center" style="margin: 5% 0 0 0;">
+      <div class="card-img-overlay">
         <h2 class="card-title text-shadow text-white text-uppercase mb-0">Featured Works</h2>
         <p class="parabooks text-shadow text-white">The works featured here have the most views/likes by fellow users of this website,
 			all of which shows aesthetics and life.
 			</p>
-        <a href="<?php echo base_url('Scrapbooks/Featured_Works'); ?>" class="btn btn-secondary">View this gallery!</a>
+        
       </div>
-    </div>
-  </div>
+     <?php foreach($featured_works as $fw): ?>
+      <div class="w3-col m4 w3-border" style="background-color: #fefefe!important;">
+        <img src = "<?php echo $fw['first_page']; ?>" style="width:80%" onclick="onClick(this)" class="pointer w3-round-xlarge" alt="The mist over the mountains">
+        <div class="w3-container">
+        <h3 class="w3-margin-top"><b><?php echo $fw['name']; ?></b></h3>
+         <h1><a class="user left" href = "<?php echo base_url('Profile/'.$fw['username']); ?>"><?php echo $fw['username']; ?></a></h1>
+          <div class="row">
+            <div class="w3-col m12" style="float: left;">          
+              <h3 >
+                <span class="glyphicon glyphicon-heart-empty left" style="color: red;"></span>
+                <span class="w3-margin-left left" style="color: #111111;"><?php echo $fw['likes']; ?></span>
+                <span class="glyphicon glyphicon-eye-open right w3-margin-left" style="color: black;"></span>
+                <span class="right"><?php echo $fw['view_counter']; ?></span>&nbsp;</h3>
+            </div>
+          </div>
+          <div>
+            <h5><i><?php echo $fw['description']; ?></i></h5>
+          </div>
+        </div>
+      </div>
+    <?php endforeach; ?> 
+<!-- end of 1st fw-->
+        <a  class="right w3-xlarge w3-black w3-hover-white  w3-wide w3-animate-opacity" href="<?php echo base_url('Scrapbooks/Featured_Works'); ?>">View this gallery!</a>
+   </div> <!--close tag -->
 
-  <div class=" p-4 my-4" style="margin:0px 85px 0px 85px;">
-    <div class="card card-inverse">
-	<div class="pic row">
-					<div class="col-md-4 mb-3 mb-md-0"> <img class="card-img-top" src = "<?php echo base_url('css/images/f1.jpg'); ?>" alt=""> </div>
-					<div class="col-md-4 mb-3 mb-md-0"> <img class="card-img-top" src = "<?php echo base_url('css/images/f2.jpg'); ?>" alt=""> </div>
-					<div class="col-md-4 mb-3 mb-md-0"> <img class="card-img-top" src = "<?php echo base_url('css/images/f3.jpg'); ?>" alt=""> </div>
-				</div>
-      <div class="card-img-overlay bg-overlay">
-        <h2 class="card-title text-shadow text-white text-uppercase mb-0">Latest Works</h2>
-        <p class="parabooks text-shadow text-white">The works featured here are the latest products from the users
-			and are rising to the top.
-			</p>
-        <a href="<?php echo base_url('Scrapbooks/Latest_Works'); ?>" class="btn btn-secondary">View this gallery!</a>
+   <div class="bgimg-5 w3-display-container w3-opacity-min">
+  <div class="w3-display-middle">
+   <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">Recent<span class="w3-hide-small">&nbsp;</span>Scrapbooks</span>
+  </div>
+</div>
+   <!-- start of Recent -->
+
+   <div class="w3-row-padding w3-center" style="margin: 5% 0 0 0;">
+      <div class="card-img-overlay">
+        <h2 class="card-title text-shadow text-white text-uppercase mb-0">Recent Works</h2>
+			        <p class="parabooks text-shadow text-white">The works featured here are the latest products from the users
+						and are rising to the top.
+						</p>
+			       
       </div>
-    </div>
+      <?php foreach($latest_works as $lw): ?>
+      <div class="w3-col m4 w3-border" style="background-color: #fefefe!important;"">
+        <img src = "<?php echo $lw['first_page']; ?>" style="width:80%" onclick="onClick(this)" class="pointer w3-round-xlarge" alt="The mist over the mountains">
+        <div class="w3-container">
+        <h3 class="w3-margin-top"><b><?php echo $lw['name']; ?></b></h3>
+         <h1><a class="user left" href = "<?php echo base_url('Profile/'.$lw['username']); ?>"><?php echo $lw['username']; ?></a></h1>
+          <div class="row">
+            <div class="w3-col m12" style="float: left;">          
+              <h3 >
+                <span class="glyphicon glyphicon-heart-empty left" style="color: red;"></span>
+                <span class="w3-margin-left left" style="color: #111111;"><?php echo $lw['likes']; ?></span>
+                <span class="glyphicon glyphicon-eye-open right w3-margin-left" style="color: black;"></span>
+                <span class="right"><?php echo $lw['view_counter']; ?></span>&nbsp;</h3>
+            </div>
+          </div>
+          <div>
+            <h5><?php echo $lw['description']; ?></h5>
+          </div>
+        </div>
+      </div>
+    <?php endforeach; ?> 
+<!-- end of 1st lw-->    
+      <a  class="right w3-xlarge w3-black w3-hover-white w3-wide w3-animate-opacity" href="<?php echo base_url('Scrapbooks/Latest_Works'); ?>">View this gallery!</a>  
+   </div> <!--close tag -->
+
+<div class="bgimg-6 w3-display-container w3-opacity-min">
+  <div class="w3-display-middle">
+    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">END <span class="w3-hide-small">OF</span> SCRAPBOOKS</span>
   </div>
+</div>
+
+<!-- Modal for full size images on click-->
+   <div id="modal01" class="w3-modal " onclick="this.style.display='none'">
+  <span class="w3-button w3-large w3-black w3-display-topright" style="margin-top: 60px;" title="Close Modal Image"><i class="fa fa-remove"></i></span>
+  <div class="w3-modal-content w3-animate-opacity w3-transparent ">
+    <img id="img01" class=" cursor w3-round-xlarge w3-image" style="width: auto;">
+    <p id="caption" class="w3-opacity w3-large"></p>
   </div>
+</div>
+
+ 
+<script type="text/javascript">
+	function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+</script>
