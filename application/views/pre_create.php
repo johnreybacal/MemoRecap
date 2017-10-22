5055d96ef51fea8bb91397b0fc37ea131771a326
Feps <30517898+feps11220@users.noreply.github.com>
2017-10-22 15:32:17 +0800

    update on pre_create

--- application/views/pre_create.php
+++ application/views/pre_create.php
@@ -1,23 +1,60 @@
-<div>
-	<h1>First things first</h1>
-	<form action = "<?php echo base_url('create'); ?>" method = "POST">
-		<br />
-		<input type="text" name="name" placeholder = "Title" required />
-		<input type="number" name="pages" placeholder = "Pages" min = "3" max = "999" required /><br />
-		<br />
-		<input type = "text" name = "description" placeholder = "description" /><br />
-		Size:<br />
-		<input type = "radio" name = "size" value = "512x768" required checked />512x768<br />
-		<input type = "radio" name = "size" value = "640x512" required />640x512<br />
-		<input type = "radio" name = "size" value = "512x512" required />512x512<br />
-		<input type = "radio" name = "size" value = "640x768" required />640x768<br />
-		<br />		
-		<?php
-			if($logged_in){
-				echo '<input type="submit" name="create" value = "Create" />';
-			}else{
-				echo '<h5>Please login first</h5>';
-			}
-		?>
-	</form>
+</div>
+	<style type="text/css">
+		body{
+			background-image: url ("<?php echo base_url('css/images/TAYTEL.png'); ?>");
+		}
+	</style>
+ <div class="container"  style="height: 700px;">
+	<div class="row">
+		<div class="col-md-8">
+				<div class="col-md-10 ">
+					<img style="width:100%; height:auto; margin:7% 0 0 0;" src = "<?php echo base_url('css/images/TAYTEL.png'); ?>" alt=""/>
+				</div>
+		</div>
+		<div class="col-md-4" style="float:right; margin:7% 0 0 0;">
+			<fieldset>								
+				<h1>First things first</h1>
+				<form action = "<?php echo base_url('create'); ?>" method = "POST">					
+					<div class="form-group">
+					    <label for="name" class="col-md-1 control-label">Title</label>
+					    <div class="col-md-12">
+							<input type="text" class="form-control" name="name" placeholder = "Title" required />
+					    </div>
+					</div>
+					<div class="form-group">
+					    <label for="name" class="col-md-1 control-label">Pages</label>
+					    <div class="col-md-12">							
+							<input type="number" class="form-control" name="pages" placeholder = "Pages" min = "1" max = "999" required />
+					    </div>
+					</div>
+					<div class="form-group">
+					    <label for="name" class="col-md-1 control-label">Description</label>
+					    <div class="col-md-12">							
+							<textarea style = "height: 80px;" class="form-control" type = "textarea" name = "description" placeholder = "description"></textarea>
+					    </div>
+					</div>
+					<div class="form-group">
+					    <label for="name" class="col-md-1 control-label">Size</label>
+					    <div class="col-md-12">
+							<input type = "radio" name = "size" value = "512x768" required checked />512x768<br />
+							<input type = "radio" name = "size" value = "640x512" required />640x512<br />
+							<input type = "radio" name = "size" value = "512x512" required />512x512<br />
+							<input type = "radio" name = "size" value = "640x768" required />640x768<br />
+					    </div>
+					</div>
+					<div class="form-group">					    
+					    <div class="col-md-12">
+							<?php
+								if($logged_in){
+									echo '<input type="submit" class = "btn btn-success" name="create" value = "Create" />';
+								}else{
+									echo '<a href = "'.base_url('Login').'">Please login first</a>';
+								}
+							?>
+						</div>
+					</div>
+				</form>
+			</fieldset>
+		</div>
+	</div>
 </div>
