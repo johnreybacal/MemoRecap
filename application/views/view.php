29fec6b2b8ed507e27196a407c56b5bdaaaef124
LoreReyes <30516891+lorejane@users.noreply.github.com>
2017-10-22 01:30:01 -0700

    Add files via upload

--- application/views/view.php
+++ application/views/view.php
@@ -22,32 +22,21 @@
 		<?php
 			echo $assignAssets;
 		?>
+				<style>
+			body{
+				background-color: #cceaee;
+				/*color:white;		*/	
+			}
+		</style>
 	</head>
 	<body onload = "onload()">
 		<div class="loader"></div>
-		<script>
-		  window.fbAsyncInit = function() {
-		    FB.init({
-		      appId      : '1969380923309252',
-		      xfbml      : true,
-		      version    : 'v2.10'
-		    });
-		    FB.AppEvents.logPageView();
-		  };
 
-		  (function(d, s, id){
-		     var js, fjs = d.getElementsByTagName(s)[0];
-		     if (d.getElementById(id)) {return;}
-		     js = d.createElement(s); js.id = id;
-		     js.src = "//connect.facebook.net/en_US/sdk.js";
-		     fjs.parentNode.insertBefore(js, fjs);
-		   }(document, 'script', 'facebook-jssdk'));
-		</script>
 		<div class="container-fluid">
 			<div class="row" style="margin: 0px auto; height:90vh;">
-				<div class="col-md-10"  class="scrollbar" id="style-1" style="overflow:auto; height:90vh;">
+				<div class="col-md-11"  class="scrollbar" id="style-1" style="overflow:auto; height:90vh;">
 					<div class="row" >
-						<div class="col-md-11" >
+						<div class="col-md-12" >
 							<div id="divContent" style="zoom: 100%">
 								<div id = "workspace" style="margin: 10% auto; position:relative; height:80vh;">
 									<?php
@@ -56,59 +45,36 @@
 								</div>
 							</div>
 						</div>
-								
-
-						<div  class="col-md-1" style="border: solid thin white;z-index:1000; position:relative; left: 20px; top: 20px">
-							<!-- <br/><br/><br/><br/><br/> -->
-						    <button type="button" value="Zoom out" OnClick="zoomOut();" class="btn btn-default btn-md"><span class="glyphicon glyphicon-minus"></span></button>
-						    <hr />
-							<button type="button" value="Orignal Size" OnClick="zoomOrig();"class="btn btn-default btn-sm">Original Size</button>
-						    <hr />
-							<button type="button" value="Zoom In" OnClick="zoomIn();" class="btn btn-default btn-md"><span class="glyphicon glyphicon-plus"></span></button>
-						</div>
+				<div style="left: 10px; top: 50vh; z-index: 999; position: fixed;">		
+							    <button type="button" value="Zoom out" OnClick="zoomOut();" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-zoom-out"></span></button>
+							    <hr />
+								<button type="button" value="Orignal Size" OnClick="zoomOrig(true);"class="btn btn-default btn-sm">Original Size</button>
+							    <hr />
+								<button type="button" value="Zoom In" OnClick="zoomIn();" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-zoom-in"></span></button>
+				</div>	
 					</div>
 				</div>
-	
-				<div class="col-md-2" style="height:90vh;">	
-					<div class="row" style="height:45vh;">
-						<div style="margin: auto;">							
-							<a href = '<?php echo base_url('editor/'.$id); ?>'  class="btn btn-success btn-lg" role="button">Edit</a>
-							<button type="button" id = 'saveAsImage' class="btn btn-info btn-lg">Download as image</button>						
-							<button type="button" id = 'shareToFB' class="btn btn-primary btn-lg">Share to facebook</button>
-						</div>							
+				<div class="col-md-1">		
+						<button class = "btn btn-warning btn-md" onclick = "back()" style="float:right;">Back</button>					
 					</div>
-					<div class="row" align="center" style="height:45vh;">
-					</div>
-						<div style="margin: auto;">
-					</div>								
-				</div>
-				<div class="row" style="height:10vh;">
-					<div id = "pagination-container" class="col-md-10" align="center">
-						<button class="btn btn-default" id = "prevPage">&laquo;</button>&nbsp;
-						<ol id = "pagination" class = "pagination">
-				
-						</ol>
-						<button class="btn btn-default" id = "nextPage">&raquo;</button>					
-					</div>	
-				</div>
 			</div>
-
-		<!-- 	<div class="container-fluid" style="height:10vh;"> -->
-				<!-- <div class="row">
-				<div class="col-md-10" align="center"> -->
-					<!-- <center>
-					<button class="btn btn-default" id = "prevPage">&laquo;</button>
-					<ol class = "pagination">
-						<?php					
-							echo $loadPagination;
-						?>				
-					</ol>
-					<button class="btn btn-default" id = "nextPage">&raquo;</button>
-					</center> -->
-			<!-- 	</div>
-				</div>
-		</div> -->
-			<!-- </div> -->	
+					<div class = "row" style="height:10vh;">				
+						<div class="col-md-11">	
+							<div class = "row" id = "pagination-container">							
+								<div class = "col-md-2" style="padding-top:25px;">
+									<button class="btn btn-default" id = "prevPage">&laquo;</button>
+								</div>
+								<div class = "col-md-8">
+										<div id = "pagination" class = "pagination"></div>
+								</div>
+								<div class = "col-md-2" style="padding-top:25px;">
+									<button  class="btn btn-default" id = "nextPage">&raquo;</button>
+								</div>							
+							</div>
+						</div>	
+				
+					</div>	
+		</div>
 		<?php					
 			echo $loadPagination;
 		?>
